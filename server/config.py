85d1ee3cab1f163050d8d93188b77c5a30f13523
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-07-26 19:06:14 -0400

    code

---
+++ server/config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+def fk_connect(conn,conn_record):
+    conn.execute("PRAGMA foreign_keys='ON'")
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///app.db"
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
